99bb7b38e940553ae0e4139edec8742105d090cd
Kyle <39416934+kajohnson6614@users.noreply.github.com>
2025-11-03 09:58:59 -0800

    CSV Files

--- Scripts/R/Combining_CSVS_Removing_Empty.R
+++ Scripts/R/Combining_CSVS_Removing_Empty.R
@@ -4,6 +4,24 @@
 source(file = "~/GitHub/Fungal_MTORC1/R_Scripts/Library_Script.R")
 
 
+
+
+Combine_Files <- function(input_path){
+  
+  file_list <- list.files(path = input_path,
+                          patter = "\\.*csv",
+                          full.names = TRUE)
+  
+  list_of_tibbles <- map(file_list, read_csv, show_col_types = FALSE)
+  filtered_tibbles <- keep(list_of_tibbles, ~nrow(.x) > 1)
+  
+  Combined_data <- bind_rows(filtered_tibbles)
+  
+  return(Combined_data)
+  
+}
+
+
 file_list <- list.files(path = "~/GitHub/AKT_Research/CSV_Files/Combined_CSVs",
                        pattern = "\\.*csv",
                        full.names = TRUE)
@@ -16,6 +34,10 @@ filtered_tibbles
 Combined_data <- bind_rows(filtered_tibbles)
 
 write_csv(Combined_data, file = "~/GitHub/AKT_Research/CSV_Files/Combined_CSVs/Combined_AKT_Potential.csv")
+
+
+
+
 
 
 
